a9a58f63d24e6e2925ec7abb1a92de01f5f443eb
Iqbal Alfaaizi <iqbal.faaizi@dkatalis.com>
2020-01-27 14:03:18 +0700

    fix category  widget

--- lib/ui/widgets/home_actions.dart
+++ lib/ui/widgets/home_actions.dart
@@ -6,55 +6,57 @@ class HomeActions extends StatefulWidget {
 }
 
 class _HomeActionsState extends State<HomeActions> {
-
   Widget _buildAction(String tag) {
     return Container(
-      width: MediaQuery.of(context).size.width * 0.22,
-      color: Colors.transparent,
-      child: Center(
+        width: MediaQuery.of(context).size.width * 0.22,
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        color: Colors.transparent,
         child: Container(
-          padding: EdgeInsets.only(left: 10, right: 10),
-          decoration: ShapeDecoration(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(32),
-            ),
-            gradient: LinearGradient(colors: [
-              Colors.red[100],
-              Colors.yellow[50]
-            ], begin: FractionalOffset.topLeft)
-          ),
-          child: Column(
-            children: <Widget>[
-              IconButton(
-                icon: Icon(
-                  tag == 'category' ? Icons.dashboard : 
-                  tag == 'travel' ? Icons.flight : 
-                  tag == 'pets' ? Icons.pets : 
-                  tag == 'phone' ? Icons.phone_android : 
-                  Icons.ac_unit,
-                  size: 30,
+            decoration: ShapeDecoration(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(32),
                 ),
-              ),
-              Text(
-                tag == 'category' ? 'Category' : 
-                tag == 'travel' ? 'Travel' : 
-                tag == 'pets' ? 'Pets' : 
-                tag == 'phone' ? 'Mobile' : 
-                'Unknown', 
-                style: TextStyle(fontSize: 11),
-              )
-            ],
-          )
-        )
-      ),
-    );
+                gradient: LinearGradient(
+                    colors: [Colors.red[100], Colors.yellow[50]],
+                    begin: FractionalOffset.topLeft)),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(
+                    tag == 'category'
+                        ? Icons.dashboard
+                        : tag == 'travel'
+                            ? Icons.flight
+                            : tag == 'pets'
+                                ? Icons.pets
+                                : tag == 'phone'
+                                    ? Icons.phone_android
+                                    : Icons.ac_unit,
+                    size: 25,
+                  ),
+                ),
+                Text(
+                  tag == 'category'
+                      ? 'Category'
+                      : tag == 'travel'
+                          ? 'Travel'
+                          : tag == 'pets'
+                              ? 'Pets'
+                              : tag == 'phone' ? 'Mobile' : 'Unknown',
+                  style: TextStyle(fontSize: 10),
+                )
+              ],
+            )));
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.only(top: 10),
-      height: MediaQuery.of(context).size.height * 0.09,
+      padding: EdgeInsets.symmetric(vertical: 10),
+      color: Colors.white,
+      height: MediaQuery.of(context).size.height * 0.11,
       child: ListView(
         scrollDirection: Axis.horizontal,
         children: <Widget>[
