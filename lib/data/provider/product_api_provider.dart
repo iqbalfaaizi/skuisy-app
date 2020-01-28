2ea8bf733cd8b804681a951bea4163408e052c34
hammsidh <hammsidhsetyawan@gmail.com>
2020-01-28 10:08:02 +0700

    prep pull from another branch

--- lib/data/provider/product_api_provider.dart
+++ lib/data/provider/product_api_provider.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:http/http.dart' show Client;
 import 'package:http/http.dart' as http;
+import 'package:skuisy_project/data/bloc/navigation_bloc.dart';
 import 'package:skuisy_project/res/strings.dart';
 import 'package:skuisy_project/data/model/product_model.dart';
 
@@ -14,5 +15,13 @@ class ProductApiProvider{
     final allProduct = productFromJson(content);
     return allProduct;
   }
+
+  // Future fetchCart() async {
+  //   String email = navBlocs.navigationProvider.currentEmail;
+  //   http.Response response =  await http.get("$base_url/getcart/$email");
+  //   await Future.delayed(Duration(milliseconds: 500));
+  //   String content = response.body;
+  //   final 
+  // }
   
 }
