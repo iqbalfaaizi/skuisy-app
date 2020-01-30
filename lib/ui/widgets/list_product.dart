892f4d383e540a764af9ba2c1d83b44aee11724b
hammsidh <hammsidhsetyawan@gmail.com>
2020-01-30 09:32:23 +0700

    prep pull dev01

--- lib/ui/widgets/list_product.dart
+++ lib/ui/widgets/list_product.dart
@@ -4,6 +4,7 @@ import 'package:skuisy_project/data/model/product_model.dart';
 import 'package:skuisy_project/ui/screens/product_details.dart';
 import 'package:getflutter/getflutter.dart';
 import 'package:intl/intl.dart';
+import 'package:skuisy_project/ui/screens/product_page.dart';
 
 class ListProduct extends StatefulWidget {
   @override
@@ -39,9 +40,7 @@ class _ListProductState extends State<ListProduct> {
         } else if (snapshot.hasError) {
           return Text(snapshot.error.toString());
         }
-        return Card(
-          child: Center(child: CircularProgressIndicator())
-        );
+        return Card(child: Center(child: CircularProgressIndicator()));
       },
     );
   }
@@ -50,49 +49,50 @@ class _ListProductState extends State<ListProduct> {
     return Container(
         padding: EdgeInsets.symmetric(horizontal: 5),
         child: Column(
-          
           children: <Widget>[
             SizedBox(height: 10),
             Container(
-              padding: EdgeInsets.symmetric(horizontal: 5),
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    flex: 1,
-                    child: Text(
-                      'Our Products',
-                      textAlign: TextAlign.start,
-                      style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                  Expanded(
-                    flex: 1,
-                    child: GestureDetector(
+                padding: EdgeInsets.symmetric(horizontal: 5),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      flex: 1,
                       child: Text(
-                        'Browse',
-                        textAlign: TextAlign.right,
-                        style: TextStyle(fontSize: 16, color: Colors.green),
+                        'Our Products',
+                        textAlign: TextAlign.start,
+                        style: TextStyle(
+                            fontSize: 22, fontWeight: FontWeight.bold),
                       ),
-                      onTap: (){print('jojo');},
-                    )
-                    
-                  ),
-                ],
-              )
-            ),
+                    ),
+                    Expanded(
+                        flex: 1,
+                        child: GestureDetector(
+                          child: Text(
+                            'Browse',
+                            textAlign: TextAlign.right,
+                            style: TextStyle(fontSize: 16, color: Colors.green),
+                          ),
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (_) => ProductPage()));
+                          },
+                        )),
+                  ],
+                )),
             SizedBox(height: 10),
             _gridBuilder(snapshot, context),
             SizedBox(height: 10),
             Container(
-              child: GFButton(
-                onPressed: (){},
-                elevation: 8.0,
-                text: 'Show more products',
-                shape: GFButtonShape.pills,
-                color: Colors.green,
-                size: GFSize.large,
-              )
-            ),
+                child: GFButton(
+              onPressed: () {},
+              elevation: 8.0,
+              text: 'Show more products',
+              shape: GFButtonShape.pills,
+              color: Colors.green,
+              size: GFSize.large,
+            )),
             SizedBox(height: 10),
           ],
         ));
@@ -106,7 +106,7 @@ Widget _gridBuilder(AsyncSnapshot snapshot, BuildContext _context) {
   final NumberFormat moneyFormat = new NumberFormat("##,##0", "en_US");
 
   return GridView.builder(
-    itemCount: /* snapshot.data.length */ 6,
+    itemCount: snapshot.data.length,
     gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
       crossAxisCount: 2,
       crossAxisSpacing: 8,
@@ -118,9 +118,8 @@ Widget _gridBuilder(AsyncSnapshot snapshot, BuildContext _context) {
       String amount = "${moneyFormat.format(_product.price)},-";
       return GestureDetector(
         child: Card(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10)
-          ),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
           elevation: 5,
           child: Container(
             child: Column(
@@ -128,64 +127,71 @@ Widget _gridBuilder(AsyncSnapshot snapshot, BuildContext _context) {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
                 Expanded(
-                  flex: 2,
-                  child: ClipRRect(
-                   borderRadius: BorderRadius.only(topLeft: Radius.circular(10), topRight: Radius.circular(10)),
-                    child: GFImageOverlay(
-                      height: 200,
-                      width: 300,
-                      colorFilter: _product.stock == 0 ? new ColorFilter.mode(Colors.black.withOpacity(0.5), BlendMode.darken) : null,
-                      image: NetworkImage(_product.picture),
-                      child: _product.stock == 0 ? Center(
-                        child: Container(
-                          padding: EdgeInsets.all(5),
-                          decoration: BoxDecoration(
-                            color: Colors.white.withOpacity(0.6),
-                            borderRadius: BorderRadius.circular(10)
-                          ),
-                          child: Text('Out of Stock', style: TextStyle(color: Colors.red)),
-                        )
-                      ) : null,
-                    )
-                  )
-                ),
+                    flex: 2,
+                    child: ClipRRect(
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(10),
+                            topRight: Radius.circular(10)),
+                        child: GFImageOverlay(
+                          height: 200,
+                          width: 300,
+                          colorFilter: _product.stock == 0
+                              ? new ColorFilter.mode(
+                                  Colors.black.withOpacity(0.5),
+                                  BlendMode.darken)
+                              : null,
+                          image: NetworkImage(_product.picture),
+                          child: _product.stock == 0
+                              ? Center(
+                                  child: Container(
+                                  padding: EdgeInsets.all(5),
+                                  decoration: BoxDecoration(
+                                      color: Colors.white.withOpacity(0.6),
+                                      borderRadius: BorderRadius.circular(10)),
+                                  child: Text('Out of Stock',
+                                      style: TextStyle(color: Colors.red)),
+                                ))
+                              : null,
+                        ))),
                 Expanded(
-                  flex: 1,
-                  child: Container(
-                    padding: EdgeInsets.all(5),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          _product.title.length < 16 ? 
-                          _product.title : _product.title.substring(0, 17) +'...',
-                          style: TextStyle(fontSize: 16),
-                          textAlign: TextAlign.left,
-                        ),
-                        SizedBox(height: 5),
-                        Text(
-                          'Rp. ${amount}',
-                          style: TextStyle(color: Colors.deepOrange, fontWeight: FontWeight.bold),
-                        ),
-                        SizedBox(height: 5),
-                        Row(
+                    flex: 1,
+                    child: Container(
+                        padding: EdgeInsets.all(5),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: <Widget>[
-                            Image(
-                              width: 18,
-                              height: 18,
-                              image: AssetImage('assets/images/online-store.png'),
-                            ),
-                            SizedBox(width: 5),
                             Text(
-                              _product.seller,
-                              style: TextStyle(color: Colors.black54),
+                              _product.title.length < 16
+                                  ? _product.title
+                                  : _product.title.substring(0, 17) + '...',
+                              style: TextStyle(fontSize: 16),
+                              textAlign: TextAlign.left,
                             ),
+                            SizedBox(height: 5),
+                            Text(
+                              'Rp. ${amount}',
+                              style: TextStyle(
+                                  color: Colors.deepOrange,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(height: 5),
+                            Row(
+                              children: <Widget>[
+                                Image(
+                                  width: 18,
+                                  height: 18,
+                                  image: AssetImage(
+                                      'assets/images/online-store.png'),
+                                ),
+                                SizedBox(width: 5),
+                                Text(
+                                  _product.seller,
+                                  style: TextStyle(color: Colors.black54),
+                                ),
+                              ],
+                            )
                           ],
-                        )
-                      ],
-                    )
-                  )
-                ),
+                        ))),
                 // Text(_product.description)
               ],
             ),
